b4ef2eaca80752e6c3bae4bca8c341b17ef25c13
Daniel Figucio <745341+affrae@users.noreply.github.com>
2021-04-22 09:09:56 +1000

    Get rid of my hello world from ghasrd.rb

--- ghasrd.rb
+++ ghasrd.rb
@@ -2,14 +2,14 @@
 
 require 'octokit'
 
-# Execute some shell command
-`pwd`
+begin
+    GITHUB_USERID = ENV.fetch("GITHUB_USERID")
+    GITHUB_PAT = ENV.fetch("GITHUB_PAT")
+  rescue KeyError
+    $stderr.puts "To run this script, please set the following environment variables:"
+    $stderr.puts "- GITHUB_USERID: Your GitHub username"
+    $stderr.puts "- GITHUB_PAT: A Personal Access Token (PAT) for your account"
+    exit 1
+  end
 
-# Was the app run successful?
-puts $?.success?
-
-# Process id of the exited app
-puts $?.pid
-
-# The actual exit status code
-puts $?.exitstatus
+  @client = nil
