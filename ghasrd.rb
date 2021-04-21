e22e8f0708705d7887b1d92923d5b5eecdc5e964
Daniel Figucio <745341+affrae@users.noreply.github.com>
2021-04-22 09:29:26 +1000

    more documentation comments

--- ghasrd.rb
+++ ghasrd.rb
@@ -3,9 +3,12 @@
 require 'octokit'
 
 # begin ... end defines code that needs to run on its own in its own context
-# rescue gives a block to execute if an error occurs during runtime
+# rescue gives a block to execute if an error occurs during runtime.
+# it functions to handle exceptions, and takes a single argument: the class/type of error that you want to rescue from.
 # in this example we are fetching environment variables.
-# If they do not exist, execute the code in the rescue block and exit status 1
+# a KeyError is raised when the specified key is not found.
+# in this case it is an environment variable
+# if an environment variable does not exist, execute the code in the rescue block and exit status 1
 # in a shell script a non-zero exit value means it is an error 
 
 begin
