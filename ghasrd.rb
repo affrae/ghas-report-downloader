bb945ee13073127934b428c2627f86aa901e5b32
Daniel Figucio <745341+affrae@users.noreply.github.com>
2021-04-22 08:37:45 +1000

    Create ghasrd.rb

---
+++ ghasrd.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+# Execute some shell command
+`pwd`
+
+# Was the app run successful?
+puts $?.success?
+
+# Process id of the exited app
+puts $?.pid
+
+# The actual exit status code
+puts $?.exitstatus
