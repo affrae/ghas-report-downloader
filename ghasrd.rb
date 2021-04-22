a1faac88a405d7a93bcb2657f77d14c2bb783eb7
Daniel Figucio <745341+affrae@users.noreply.github.com>
2021-04-22 10:50:15 +1000

    added options parsing

--- ghasrd.rb
+++ ghasrd.rb
@@ -1,7 +1,61 @@
 #!/usr/bin/ruby
 
 require 'octokit'
+require 'optparse'
+require 'optparse/time'
+require 'ostruct'
+require 'pp'
 
+# Get command Line options using docs @ https://ruby-doc.org/stdlib-2.1.1/libdoc/optparse/rdoc/OptionParser.html
+
+class Optparse
+
+    def self.parse(args)
+      # The options specified on the command line will be collected in *options*.
+      # We set default values here.
+      options = OpenStruct.new
+      options.verbose = false
+  
+      opt_parser = OptionParser.new do |opts|
+        opts.banner = "Usage: ghasrd.rb [options]"
+  
+        opts.separator ""
+        opts.separator "Specific options:"
+  
+        # List the reports available
+        opts.on("-l", "--list",
+                "List the reports avilable") do
+          options.command = "list"
+        end
+        
+        # get or grab one or more reports
+
+        opts.on("-g x,y,z", "--get x,y,z", "--grab x,y,z", Array, "Get one or more reports by analysis_id") do |reportList|
+           options.reportList = reportList
+        end
+  
+        # Run verbosely
+        opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+           options.verbose = v
+        end
+  
+        opts.separator ""
+        opts.separator "Common options:"
+  
+        # No argument, shows at tail.  This will print an options summary.
+        # Try it and see!
+        opts.on_tail("-h", "--help", "Show this message") do
+          puts opts
+          exit
+        end
+      end
+  
+      opt_parser.parse!(args)
+      options
+    end  # parse()
+  
+  end  # class Optparse
+  
 # begin ... end defines code that needs to run on its own in its own context
 # rescue gives a block to execute if an error occurs during runtime.
 # it functions to handle exceptions, and takes a single argument: the class/type of error that you want to rescue from.
@@ -19,6 +73,11 @@ rescue KeyError
     $stderr.puts "- GITHUB_USERID: Your GitHub username"
     $stderr.puts "- GITHUB_PAT: A Personal Access Token (PAT) for your account"
     exit 1
+end
+
+options = Optparse.parse(ARGV)
+if options.verbose then
+    pp options
 end
 
 @client = nil
