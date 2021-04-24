341dba4020ca3fcd725ea760b584b1a25c18606b
Daniel Figucio <745341+affrae@users.noreply.github.com>
2021-04-25 09:03:52 +1000

    more long line fixes

--- ghasrd.rb
+++ ghasrd.rb
@@ -35,7 +35,9 @@ class Optparse
       opts.on('-o', '--owner OWNER', '(Required) the OWNER of the repository') do |owner|
         unless owner.match('^([a-z0-9])(?!.*--)([a-z0-9-])*([a-z0-9])$')
           raise OptionParser::InvalidArgument,
-                "OWNER may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen. '#{owner}' fails this test!"
+                'OWNER may only contain alphanumeric characters or single hyphens,' \
+                ' and cannot begin or end with a hyphen.' \
+                " '#{owner}' fails this test!"
         end
 
         options.owner = owner
